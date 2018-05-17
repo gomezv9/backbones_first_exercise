f8ec8d44d5b778257acc2947d0a0ad781642d8b5
Jaime Gomez <jaime.gomez@globalgaming.com>
2018-05-17 16:33:44 +0300

    test pull request

--- view_model_collection-Basic/js/main.js
+++ view_model_collection-Basic/js/main.js
@@ -2,7 +2,7 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
-
+// test
 var Song = Backbone.Model.extend();
 
 var Songs = Backbone.Collection.extend({
